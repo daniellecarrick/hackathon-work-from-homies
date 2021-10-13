8a8051f4af811a3a63e5576413b7997df15a05ad
Danielle Carrick <dacarrick@gmail.com>
2021-10-13 07:24:27 -0400

    style updates on mentor section

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -4,16 +4,22 @@ import narcisa from "../assets/images/narcisa.png";
 import danielle from "../assets/images/danielle.png";
 
 const styles = {
-  mentor: { display: "flex" },
-  image: { width: "150px", height: "150px" },
-
+  mentor: {
+    display: "flex",
+    flex: "30%",
+    flexDirection: "column",
+    margin: "0 20px 20px 0",
+  },
+  name: { fontWeight: "bold" },
+  image: { width: "100%", height: "200px" },
+  meet: { display: "flex", flexWrap: "wrap" },
   section: {
-    height: "100vh",
     display: "flex",
     padding: "50px",
     alignItems: "center",
     flexDirection: "column",
     fontFamily: "Arial",
+    backgroundColor: "#efefef",
   },
 };
 
@@ -49,11 +55,12 @@ const data = [
     text: "I enjoy using my analytical thinking and problem solving skills to contribute to better decision-making processes. My appetite for details leads me to very exciting investigations and interesting findings every day.",
   },
 ];
+
 const Mentors = ({ classes }) => {
   return (
     <section className={classes.section}>
+      <h1>Meet your Mentors</h1>
       <div className={classes.meet}>
-        <h1>Meet your Mentors</h1>
         {data.map((mentor) => {
           return (
             <div className={classes.mentor}>
@@ -67,11 +74,9 @@ const Mentors = ({ classes }) => {
               <p>
                 <span className={classes.name}>
                   {mentor.name}, {mentor.title}
-                </span>{" "}
-                <br />I enjoy using my analytical thinking and problem solving
-                skills to contribute to better decision-making processes. My
-                appetite for details leads me to very exciting investigations
-                and interesting findings every day.
+                </span>
+                <br />
+                {mentor.text}
               </p>
             </div>
           );
